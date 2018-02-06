6eaa91f66c2605d856939fe9b4b7d1abdd44845e
denisk <den.kasakov@gmail.com>
2018-02-06 15:40:34 +0200

    Logging engine changed to NLog. Output to Activity Log UI moved to DHSCore project. PinDominator temporary removed from solution to applying logging there.

---
+++ DominatorHouseCore/Utility/ConstantVariable.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Documents;
+using DominatorHouseCore.Enums;
+
+namespace DominatorHouseCore.Utility
+{
+    public static class ConstantVariable
+    {
+
+        public static string UseragentCommonFormat { get; } = "Instagram {0} Android ({1}/{2}; {3}; {4}; {5}; {6}; {7}; {8}; {9})";
+
+        public static string UseragentLocale { get; } = "en_US";
+
+        public static string IgVersion { get; } = "10.33.0";
+
+        public static string ApiUrl => $"{(object)ConstantVariable.InstagramBaseUrl}api/v1/";
+
+        public static string InstagramBaseUrl { get; } = "https://i.instagram.com/";
+
+        public static string XFbHttpEngine { get; } = "Liger";
+
+        public static int WebrequestTimeout { get; } = 20000;
+
+        public static bool UseSystemProxy { get; } = true;
+
+        public static string Manufacturer { get; } = "DOMINATORHOUSE";
+
+      //  public static string GdPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\{Manufacturer}\\GD";
+
+
+        public static string GetDominatorPath(SocialNetworks SocialNetworkType)
+        {
+            string DirectoryName = string.Empty;
+
+            switch (SocialNetworkType)
+            {
+                case SocialNetworks.Instagram:
+                    DirectoryName = "GD";
+                    break;
+                default:
+                        DirectoryName = SocialNetworkType.ToString();
+                        break;
+            }
+
+            return $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\{Manufacturer}\\{DirectoryName}" ;
+        }
+
+
+     //   public static string GetConfigurationPath(SocialNetworks.Instagram) = $"{GdPath}\\Configurations";
+
+
+        public static string GetConfigurationPath(SocialNetworks SocialNetworkType)
+        {
+            return $"{GetDominatorPath(SocialNetworkType)}\\Configurations";
+        }
+
+        public const string TemplateBinName = "Template.bin";
+
+        public const string CreateCampaign = "Create Campaign";
+
+        public const string UpdateCampaign = "Update Campaign";
+
+        public const string NoAccountSelected = "No Account Selected";
+
+
+        public static string GetIndexAccountPath(SocialNetworks SocialNetworkType)
+        {
+            return GetDominatorPath(SocialNetworkType) + "\\Index\\AC";
+        }
+
+       // public static string GetIndexCampaignPath(SocialNetworks.Instagram) = GdPath + "\\Index\\CP";
+
+        public static string GetIndexCampaignPath(SocialNetworks SocialNetworkType)
+        {
+            return GetDominatorPath(SocialNetworkType) + "\\Index\\CP";
+        }
+
+
+
+        public static string AccountDetails = "AccountDetails.bin";
+
+        public static string CampaignDetails = "CampaignDetails.bin";
+
+        public const string UnGrouped = "Ungrouped";
+
+        public const string NotYetUpdate = "Not yet update";
+
+        public const string NotChecked = "Not Checked";
+
+        public static string DateasFileName { get; set; } = DateTime.Now.ToString("ddMMyyyyHmmss");
+
+
+      //  public static string socialConfigurationPath = new SocialNetworkPathFactory(SocialNetworks.Instagram).GetSocialNetworkConfigPath();
+
+
+        public static string socialConfigurationPath(SocialNetworks SocialNetworkType)
+        {
+           return new SocialNetworkPathFactory(SocialNetworkType).GetSocialNetworkConfigPath();
+        }
+
+     //   public static string socialNetworkPath = new SocialNetworkPathFactory(SocialNetworks.Instagram).GetSocialNetworkCampaignPath();
+
+        public static string socialNetworkPath(SocialNetworks SocialNetworkType)
+        {
+            return new SocialNetworkPathFactory(SocialNetworkType).GetSocialNetworkCampaignPath();
+        }
+
+    }
+}
