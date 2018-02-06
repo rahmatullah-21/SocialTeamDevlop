2430444998ea8de990b066be7c1a2c6ca0dfe828
vikas singh <vikassingh@globussoft.in>
2018-02-06 13:13:15 +0530

    adding dropdown for social network selection

---
+++ DominatorHouse/MainWindowOld.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DominatorHouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowOld : Window
+    {
+        public MainWindowOld()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            GramDominatorUI.MainWindow GDWindows = new GramDominatorUI.MainWindow();
+            GDWindows.Show();
+        }
+
+        private void ButtonTD_OnClick(object sender, RoutedEventArgs e)
+        {
+            TwtDominatorUI.MainWindow TdWindows = new TwtDominatorUI.MainWindow();
+            TdWindows.Show();
+        }
+    }
+}
