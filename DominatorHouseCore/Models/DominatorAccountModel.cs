3e646e89ef8aa28e84daaa8b7c406bae505074b6
Sathishkumar <satishkumar@globussoft.in>
2018-02-21 19:50:56 +0530

    Added the custom control for display the account details with respective module

--- DominatorHouseCore/Models/DominatorAccountModel.cs
+++ DominatorHouseCore/Models/DominatorAccountModel.cs
@@ -35,7 +35,6 @@ namespace DominatorHouseCore.Models
             }
         }
 
-
         #region Common Properties
 
         [ProtoMember(2)]
--- DominatorHouseCore/Utility/ModuleConfiguration.cs
+++ DominatorHouseCore/Utility/ModuleConfiguration.cs
@@ -10,16 +10,24 @@ namespace DominatorHouseCore.Utility
     {
         [ProtoMember(1)]
         public string TemplateId { get; set; }
+
         [ProtoMember(2)]
         public bool IsEnabled { get; set; }
+
         [ProtoMember(3)]
         public string Status { get; set; }
+
         [ProtoMember(4)]
         public int LastUpdatedDate { get; set; } = DateTimeUtilities.GetEpochTime();
+
         [ProtoMember(5)]
         public List<RunningTimes> LstRunningTimes { get; set; }
 
         [ProtoMember(6)]
         public ActivityType ActivityType { get; set; }
+
+        [ProtoMember(7)]
+        public int MaximumCountPerDay { get; set; } = 30;
+
     }
 }
