f74c4c4da8b45aa8dbdd4ab1b66cdc95f708ab92
Sathishkumar <satishkumar@globussoft.in>
2018-03-13 13:36:27 +0530

    commit before Checkout

--- DominatorHouseCore/Models/QueryInfo.cs
+++ DominatorHouseCore/Models/QueryInfo.cs
@@ -238,6 +238,12 @@ namespace DominatorHouseCore.Models
         [Description("langUsersWhoLiked")]
         UsersWhoLikedPost = 13,
         [Description("langUsersWhoCommented")]
-        UsersWhoCommentedOnPost = 14
+        UsersWhoCommentedOnPost = 14,
+        [Description("langFromSomeonesCircle")]
+        FromSomeonesCircle=15,
+        [Description("langFromCircleOfFollowers")]
+        FromCircleOfFollowers=16,
+        [Description("langFromCircleOfFollowings")]
+        FromCircleOfFollowings=17
     }
 }
