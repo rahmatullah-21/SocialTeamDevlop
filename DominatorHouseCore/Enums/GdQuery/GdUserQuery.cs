f610d7b163818aa407cdb9a6f1fcea14fa5e0a4f
Sathishkumar <satishkumar@globussoft.in>
2018-04-16 17:53:47 +0530

    Added Gd Query

---
+++ DominatorHouseCore/Enums/GdQuery/GdUserQuery.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace DominatorHouseCore.Enums.GdQuery
+{
+    public enum GdUserQuery
+    {
+        [Description("langKeywords")]
+        Keywords,
+        [Description("langOwnFollowers")]
+        OwnFollowers,
+        [Description("langOwnFollowing")]
+        OwnFollowing,
+        [Description("langSuggestedUsers")]
+        SuggestedUsers,
+        [Description("langHashtagusers")]
+        HashtagUsers,
+        [Description("langSomeonesFollowers")]
+        SomeonesFollowers,
+        [Description("langSomeonesFollowings")]
+        SomeonesFollowings,
+        [Description("langFollowersOfSomeonesFollowers")]
+        FollowersOfFollowers,
+        [Description("langFollowersOfSomeonesFollowings")]
+        FollowersOfFollowings,
+        [Description("langLocationUsers")]
+        LocationUsers,
+        [Description("langCustomUser")]
+        CustomUsers,
+        [Description("langUsersWhoLiked")]
+        UsersWhoLikedPost,
+        [Description("langUsersWhoCommented")]
+        UsersWhoCommentedOnPost,
+    }
+}
