13a036d30353a5bdcdde74800c655c3d4d46e376
Sathishkumar <satishkumar@globussoft.in>
2018-05-01 11:48:48 +0530

    Changed the assembly name

--- DominatorHouse/Properties/AssemblyInfo.cs
+++ DominatorHouse/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DominatorHouse")]
+[assembly: AssemblyTitle("Socinator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DominatorHouse")]
+[assembly: AssemblyProduct("Socinator")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
