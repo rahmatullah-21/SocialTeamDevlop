b0e4abbfdb01e045c52944520cf3f4a86f0ec8d8
kamalglb <kamalkumar@globussoft.com>
2018-05-11 16:54:38 +0530

    ** Removed DateTimeHelper Class

--- DominatorHouseCore/Converters/EpochToDateTimeConverter.cs
+++ DominatorHouseCore/Converters/EpochToDateTimeConverter.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 using DominatorHouse.UsefullUtilitiesLibrary;
+using DominatorHouseCore.Utility;
 
 namespace DominatorHouseCore.Converters
 {
@@ -13,7 +14,7 @@ namespace DominatorHouseCore.Converters
         {
             try
             {
-                return DateTimeHelper.EpochToDateTimeUtc(int.Parse(value.ToString()));
+                return DateTimeUtilities.EpochToDateTimeUtc(int.Parse(value.ToString()));
             }
             catch (Exception)
             {
