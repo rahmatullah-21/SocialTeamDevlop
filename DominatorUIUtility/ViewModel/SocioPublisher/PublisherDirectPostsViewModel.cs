ab932ceafe08e86f5595ccf0cdb5406b8760820d
Sathishkumar <satishkumar@globussoft.in>
2018-06-06 20:54:26 +0530

    Adding multiple post UI

--- DominatorUIUtility/ViewModel/SocioPublisher/PublisherDirectPostsViewModel.cs
+++ DominatorUIUtility/ViewModel/SocioPublisher/PublisherDirectPostsViewModel.cs
@@ -1,5 +1,12 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using DominatorHouseCore;
 using DominatorHouseCore.Command;
+using DominatorHouseCore.Utility;
+using DominatorUIUtility.Views.SocioPublisher;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace DominatorUIUtility.ViewModel.SocioPublisher
 {
@@ -27,7 +34,37 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
 
         public void ExecuteMultiPost(object sender)
         {
-                     
+            try
+            {
+                var publisherMultiplePost = new PublisherMultiplePost
+                    {                       
+                        ShowInTaskbar = true,
+                        ShowActivated = true,
+                        Topmost = false,
+                        ResizeMode = ResizeMode.NoResize,
+                        WindowStyle = WindowStyle.SingleBorderWindow,
+                        WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                        ShowTitleBar = true,
+                        ShowCloseButton = true,
+                        WindowTransitionsEnabled = false,                      
+                        BorderThickness = new Thickness(0),
+                        GlowBrush = Brushes.Black,
+                };
+                publisherMultiplePost.Show();
+
+                //var customDialog = new CustomDialog
+                //{
+                //    HorizontalAlignment = HorizontalAlignment.Center,
+                //    Content = publisherMultiplePost
+                //};
+                //var objDialog = new Dialog();
+                //var dialogWindow = objDialog.GetCustomDialog(customDialog, "Multiple Postlist");
+                //dialogWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                ex.DebugLog();
+            }
         }
         #endregion
 
