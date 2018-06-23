297b415165d33c8346fb0b7eeb8681051f6c2fa0
kumarharsh <kumarharsh@globussoft.in>
2018-06-23 13:38:58 +0530

    Added functionality for checked select all if manually selceted all post and unchecked select all if any one is unchecked binded command for select manualy

--- DominatorUIUtility/ViewModel/SocioPublisher/PublisherManageDestinationViewModel.cs
+++ DominatorUIUtility/ViewModel/SocioPublisher/PublisherManageDestinationViewModel.cs
@@ -86,11 +86,7 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
                 if (_isAllDestinationSelected == value)
                     return;
                 SetProperty(ref _isAllDestinationSelected, value);
-
-                //if (_isAllDestinationSelected)
-                //    SelectAllDestination();
-                //else
-                //    SelectNoneDestination();
+               
                 SelectAllDestination(_isAllDestinationSelected);
                 _isUncheckedFromList = false;
             }
@@ -104,22 +100,7 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
                 x.IsSelected = isAllSelected; return x;
             }).ToList();
         }
-        //public void SelectAllDestination()
-        //{
-        //    ListPublisherManageDestinationModels.Select(x =>
-        //    {
-        //        x.IsSelected = true; return x;
-        //    }).ToList();
-        //}
-
-        //public void SelectNoneDestination()
-        //{
-        //    ListPublisherManageDestinationModels.Select(x =>
-        //    {
-        //        x.IsSelected = false; return x;
-        //    }).ToList();
-        //}
-
+       
 
         private bool NavigationCanExecute(object sender) => true;
 
