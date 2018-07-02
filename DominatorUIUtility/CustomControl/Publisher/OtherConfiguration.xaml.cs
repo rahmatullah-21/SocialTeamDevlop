e9f248c174863e597e4d549bc850a4f0c4d6ccfb
Sathishkumar <satishkumar@globussoft.in>
2018-07-02 12:03:45 +0530

    Fixed Re-add issues, Fixed Gplus advanced settings issues, Fixed Import Csv files issues, Fixed Multiple images issues

--- DominatorUIUtility/CustomControl/Publisher/OtherConfiguration.xaml.cs
+++ DominatorUIUtility/CustomControl/Publisher/OtherConfiguration.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DominatorHouseCore;
 using DominatorHouseCore.Enums;
 using DominatorHouseCore.FileManagers;
 using DominatorHouseCore.LogHelper;
@@ -60,145 +61,152 @@ namespace DominatorUIUtility.CustomControl.Publisher
 
             ObjCampaignsAdvanceSetting.BtnSave.Click += (senders, args) =>
             {
-                var campaignId = ObjCampaignsAdvanceSetting.AdvanceSetting.CampaignId;
-
-                #region General
-
-                var oldGeneralModel = ObjCampaignsAdvanceSetting.AdvanceSetting.GeneralModel;
-                var newGeneralModel = General.GetSingeltonGeneralObject().GeneralViewModel.GeneralModel;
-                newGeneralModel = ObjectComparer.CompareAndGetChangedObject<GeneralModel>(oldGeneralModel, newGeneralModel);
-                if (newGeneralModel != null)
+                try
                 {
-                    newGeneralModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Social);
-                    var generalModels = GenericFileManager.GetModuleDetails<GeneralModel>(file);
-                    var moduleToUpdate = generalModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newGeneralModel, generalModels, file, SocialNetworks.Social);
+                    var campaignId = ObjCampaignsAdvanceSetting.AdvanceSetting.CampaignId;
+
+                    #region General
+
+                    var oldGeneralModel = ObjCampaignsAdvanceSetting.AdvanceSetting.GeneralModel;
+                    var newGeneralModel = General.GetSingeltonGeneralObject().GeneralViewModel.GeneralModel;
+                    newGeneralModel = ObjectComparer.CompareAndGetChangedObject<GeneralModel>(oldGeneralModel, newGeneralModel);
+                    if (newGeneralModel != null)
+                    {
+                        newGeneralModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Social);
+                        var generalModels = GenericFileManager.GetModuleDetails<GeneralModel>(file);
+                        var moduleToUpdate = generalModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newGeneralModel, generalModels, file, SocialNetworks.Social);
+                    }
+
+                    #endregion
+
+                    #region FaceBook
+
+                    var oldFacebookModel = ObjCampaignsAdvanceSetting.AdvanceSetting.FacebookModel;
+                    var newFacebookModel = Facebook.GetSingeltonFacebookObject().FacebookViewModel.FacebookModel;
+                    newFacebookModel = ObjectComparer.CompareAndGetChangedObject<FacebookModel>(oldFacebookModel, newFacebookModel);
+                    if (newFacebookModel != null)
+                    {
+                        newFacebookModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Facebook);
+                        var lstFacebookModels = GenericFileManager.GetModuleDetails<FacebookModel>(file);
+                        var moduleToUpdate = lstFacebookModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newFacebookModel, lstFacebookModels, file, SocialNetworks.Facebook);
+                    }
+
+                    #endregion
+
+                    #region Google+
+
+                    var oldGooglePlusModel = ObjCampaignsAdvanceSetting.AdvanceSetting.GooglePlusModel;
+                    var newGooglePlusModel = GooglePlus.GetSingeltonGooglePlusObject().GooglePlusViewModel.GooglePlusModel;
+                    newGooglePlusModel = ObjectComparer.CompareAndGetChangedObject<GooglePlusModel>(oldGooglePlusModel, newGooglePlusModel);
+
+                    if (newGooglePlusModel != null)
+                    {
+                        newGooglePlusModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Gplus);
+                        var lstGooglePlusModels = GenericFileManager.GetModuleDetails<GooglePlusModel>(file);
+                        var moduleToUpdate = lstGooglePlusModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newGooglePlusModel, lstGooglePlusModels, file, SocialNetworks.Gplus);
+                    }
+                    #endregion
+
+                    #region Instagram
+
+                    var oldInstagramModel = ObjCampaignsAdvanceSetting.AdvanceSetting.InstagramModel;
+                    var newInstagramModel = Instagram.GetSingeltonInstagramObject().InstagramViewModel.InstagramModel;
+                    newInstagramModel =
+                        ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.InstagramModel>(oldInstagramModel,
+                            newInstagramModel);
+                    if (newInstagramModel != null)
+                    {
+                        newInstagramModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Instagram);
+                        var lstInstagramModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.InstagramModel>(file);
+                        var moduleToUpdate = lstInstagramModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newInstagramModel, lstInstagramModels, file, SocialNetworks.Instagram);
+                    }
+
+                    #endregion
+
+                    #region Pinterest
+
+                    var oldPinterestModel = ObjCampaignsAdvanceSetting.AdvanceSetting.PinterestModel;
+                    var newPinterestModel = Pinterest.GetSingeltonPinterestObject().PinterestViewModel.PinterestModel;
+                    newPinterestModel = ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.PinterestModel>(oldPinterestModel, newPinterestModel);
+
+                    if (newPinterestModel != null)
+                    {
+                        newPinterestModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Pinterest);
+                        var lstPinterestModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.PinterestModel>(file);
+                        var moduleToUpdate = lstPinterestModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newPinterestModel, lstPinterestModels, file, SocialNetworks.Pinterest);
+                    }
+                    #endregion
+
+                    #region Tumblr
+
+                    var oldTumblrModel = ObjCampaignsAdvanceSetting.AdvanceSetting.TumblrModel;
+                    var newTumblrModel = Tumblr.GetSingeltonTumblr().TumblrViewModel.TumblrModel;
+                    newTumblrModel =
+                        ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TumblrModel>(oldTumblrModel,
+                            newTumblrModel);
+                    if (newTumblrModel != null)
+                    {
+                        newTumblrModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Tumblr);
+                        var lstTumblrModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TumblrModel>(file);
+                        var moduleToUpdate = lstTumblrModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newTumblrModel, lstTumblrModels, file, SocialNetworks.Tumblr);
+                    }
+
+                    #endregion
+
+                    #region Twitter
+
+                    var oldTwitterModel = ObjCampaignsAdvanceSetting.AdvanceSetting.TwitterModel;
+                    var newTwitterModel = Twitter.GetSingletonTwitterObject().TwitterViewModel.TwitterModel;
+                    newTwitterModel = ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TwitterModel>(oldTwitterModel, newTwitterModel);
+
+                    if (newTwitterModel != null)
+                    {
+                        newTwitterModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Twitter);
+                        var lstTwitterModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TwitterModel>(file);
+                        var moduleToUpdate = lstTwitterModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newTwitterModel, lstTwitterModels, file, SocialNetworks.Twitter);
+                    }
+
+                    #endregion
+
+                    #region Reddit
+
+                    var oldRedditModel = ObjCampaignsAdvanceSetting.AdvanceSetting.RedditModel;
+                    var newRedditModel = Reddit.GetSingeltonRedditObject().RedditViewModel.RedditModel;
+                    newRedditModel = ObjectComparer.CompareAndGetChangedObject<RedditModel>(oldRedditModel, newRedditModel);
+
+                    if (newRedditModel != null)
+                    {
+                        newRedditModel.CampaignId = campaignId;
+                        var file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Reddit);
+                        var lstRedditModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.RedditModel>(file);
+                        var moduleToUpdate = lstRedditModels.FirstOrDefault(x => x.CampaignId == campaignId);
+                        ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newRedditModel, lstRedditModels, file, SocialNetworks.Reddit);
+                    }
+
+                    #endregion
+
+                    GlobusLogHelper.log.Info("Details successfully saved");
+
+                    window.Close();
                 }
-
-                #endregion
-
-                #region FaceBook
-
-                var oldFacebookModel = ObjCampaignsAdvanceSetting.AdvanceSetting.FacebookModel;
-                var newFacebookModel = Facebook.GetSingeltonFacebookObject().FacebookViewModel.FacebookModel;
-                newFacebookModel = ObjectComparer.CompareAndGetChangedObject<FacebookModel>(oldFacebookModel, newFacebookModel);
-                if (newFacebookModel != null)
+                catch (Exception ex)
                 {
-                    newFacebookModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Facebook);
-                    var lstFacebookModels = GenericFileManager.GetModuleDetails<FacebookModel>(file);
-                    var moduleToUpdate = lstFacebookModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting. AddUpdateDetails(moduleToUpdate, newFacebookModel, lstFacebookModels, file, SocialNetworks.Facebook);
+                    ex.DebugLog();
                 }
-
-                #endregion
-
-                #region Google+
-
-                var oldGooglePlusModel = ObjCampaignsAdvanceSetting.AdvanceSetting.GooglePlusModel;
-                var newGooglePlusModel = GooglePlus.GetSingeltonGooglePlusObject().GooglePlusViewModel.GooglePlusModel;
-                newGooglePlusModel = ObjectComparer.CompareAndGetChangedObject<GooglePlusModel>(oldGooglePlusModel, newGooglePlusModel);
-
-                if (newGooglePlusModel != null)
-                {
-                    newGeneralModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Gplus);
-                    var lstGooglePlusModels = GenericFileManager.GetModuleDetails<GooglePlusModel>(file);
-                    var moduleToUpdate = lstGooglePlusModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newGooglePlusModel, lstGooglePlusModels, file, SocialNetworks.Gplus);
-                }
-                #endregion
-
-                #region Instagram
-
-                var oldInstagramModel = ObjCampaignsAdvanceSetting.AdvanceSetting.InstagramModel;
-                var newInstagramModel = Instagram.GetSingeltonInstagramObject().InstagramViewModel.InstagramModel;
-                newInstagramModel =
-                    ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.InstagramModel>(oldInstagramModel,
-                        newInstagramModel);
-                if (newInstagramModel != null)
-                {
-                    newInstagramModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Instagram);
-                    var lstInstagramModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.InstagramModel>(file);
-                    var moduleToUpdate = lstInstagramModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newInstagramModel, lstInstagramModels, file, SocialNetworks.Instagram);
-                }
-
-                #endregion
-
-                #region Pinterest
-
-                var oldPinterestModel = ObjCampaignsAdvanceSetting.AdvanceSetting.PinterestModel;
-                var newPinterestModel = Pinterest.GetSingeltonPinterestObject().PinterestViewModel.PinterestModel;
-                newPinterestModel = ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.PinterestModel>(oldPinterestModel, newPinterestModel);
-
-                if (newPinterestModel != null)
-                {
-                    newPinterestModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Pinterest);
-                    var lstPinterestModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.PinterestModel>(file);
-                    var moduleToUpdate = lstPinterestModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newPinterestModel, lstPinterestModels, file, SocialNetworks.Pinterest);
-                }
-                #endregion
-
-                #region Tumblr
-
-                var oldTumblrModel = ObjCampaignsAdvanceSetting.AdvanceSetting.TumblrModel;
-                var newTumblrModel = Tumblr.GetSingeltonTumblr().TumblrViewModel.TumblrModel;
-                newTumblrModel =
-                    ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TumblrModel>(oldTumblrModel,
-                        newTumblrModel);
-                if (newTumblrModel != null)
-                {
-                    newTumblrModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Tumblr);
-                    var lstTumblrModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TumblrModel>(file);
-                    var moduleToUpdate = lstTumblrModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newTumblrModel, lstTumblrModels, file, SocialNetworks.Tumblr);
-                }
-
-                #endregion
-
-                #region Twitter
-
-                var oldTwitterModel = ObjCampaignsAdvanceSetting.AdvanceSetting.TwitterModel;
-                var newTwitterModel = Twitter.GetSingletonTwitterObject().TwitterViewModel.TwitterModel;
-                newTwitterModel = ObjectComparer.CompareAndGetChangedObject<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TwitterModel>(oldTwitterModel, newTwitterModel);
-
-                if (newTwitterModel != null)
-                {
-                    newTwitterModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Twitter);
-                    var lstTwitterModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.TwitterModel>(file);
-                    var moduleToUpdate = lstTwitterModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newTwitterModel, lstTwitterModels, file, SocialNetworks.Twitter);
-                }
-
-                #endregion
-
-                #region Reddit
-
-                var oldRedditModel = ObjCampaignsAdvanceSetting.AdvanceSetting.RedditModel;
-                var newRedditModel = Reddit.GetSingeltonRedditObject().RedditViewModel.RedditModel;
-                newRedditModel = ObjectComparer.CompareAndGetChangedObject<RedditModel>(oldRedditModel, newRedditModel);
-
-                if (newRedditModel != null)
-                {
-                    newRedditModel.CampaignId = campaignId;
-                    string file = ConstantVariable.GetPublisherOtherConfigFile(SocialNetworks.Reddit);
-                    var lstRedditModels = GenericFileManager.GetModuleDetails<DominatorHouseCore.Models.Publisher.CampaignsAdvanceSetting.RedditModel>(file);
-                    var moduleToUpdate = lstRedditModels.FirstOrDefault(x => x.CampaignId == campaignId);
-                    ObjCampaignsAdvanceSetting.AddUpdateDetails(moduleToUpdate, newRedditModel, lstRedditModels, file, SocialNetworks.Reddit);
-                }
-
-                #endregion
-
-                GlobusLogHelper.log.Info("Details successfully saved");
-
-                window.Close();
             };
 
             window.Show();
--- DominatorUIUtility/ViewModel/SocioPublisher/PublisherPostlistBaseViewModel.cs
+++ DominatorUIUtility/ViewModel/SocioPublisher/PublisherPostlistBaseViewModel.cs
@@ -617,8 +617,11 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
 
                         postItems.PublishedTriedAndSuccessStatus = $"{triedCount}/{successCount}";
 
-                        PublisherPostlist.Add(postItems);
-                        PostCollectionView = CollectionViewSource.GetDefaultView(PublisherPostlist);
+                        if (PublisherPostlist.All(x => x.PostId != postItems.PostId))
+                        {
+                            PublisherPostlist.Add(postItems);
+                            PostCollectionView = CollectionViewSource.GetDefaultView(PublisherPostlist);
+                        }
 
                         if (PublisherPostlist.Count == PostCount)
                             IsProgressRingActive = false;
@@ -695,39 +698,48 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
 
         private void ChangePostStatusExecute(object sender)
         {
-            var statusToChange = ((Button)sender).Content;
-            var campaignStatus = ((FrameworkElement)sender).DataContext as PublisherPostlistModel;
-
-            if (campaignStatus == null)
-                return;
-
-            var post = PostlistFileManager.GetByPostId(campaignStatus.CampaignId, campaignStatus.PostId);
-
-            switch (statusToChange.ToString())
+            try
             {
-                case "Publish Now":
-                    campaignStatus.PostQueuedStatus = PostQueuedStatus.Published;
+                var statusToChange = ((Button)sender).Content;
+                var publisherPostlistModel = ((FrameworkElement)sender).DataContext as PublisherPostlistModel;
+
+                if (publisherPostlistModel == null)
+                    return;
+
+                var post = PostlistFileManager.GetByPostId(publisherPostlistModel.CampaignId, publisherPostlistModel.PostId);
+
+                if (statusToChange.ToString() == (string)Application.Current.FindResource("LangKeyPublishNow"))
+                { 
+                    publisherPostlistModel.PostQueuedStatus = PostQueuedStatus.Published;
                     post.PostQueuedStatus = PostQueuedStatus.Published;
-                    PostProcessOfStatusChange(campaignStatus, post);
-                    break;
-                case "Re-add":
+                    PostProcessOfStatusChange(publisherPostlistModel, post);
+                    PublishNow(publisherPostlistModel);
+                }
+                else if (statusToChange.ToString() == (string) Application.Current.FindResource("LangKeyReAdd"))
+                {
                     var readdPost = post.DeepClone();
                     readdPost.GenerateClonePostId();
                     readdPost.PostQueuedStatus = PostQueuedStatus.Pending;
                     readdPost.LstPublishedPostDetailsModels = new ObservableCollection<PublishedPostDetailsModel>();
                     PostlistFileManager.Add(readdPost.CampaignId, readdPost);
-                    PublisherInitialize.GetInstance.UpdatePostStatus(campaignStatus.CampaignId);
-                    break;
-                case "Send to Pending":
-                    campaignStatus.PostQueuedStatus = PostQueuedStatus.Pending;
+                    PublisherInitialize.GetInstance.UpdatePostStatus(publisherPostlistModel.CampaignId);
+                }
+                else if (statusToChange.ToString() == (string)Application.Current.FindResource("LangKeySendToPending"))
+                {
+                    publisherPostlistModel.PostQueuedStatus = PostQueuedStatus.Pending;
                     post.PostQueuedStatus = PostQueuedStatus.Pending;
-                    PostProcessOfStatusChange(campaignStatus, post);
-                    break;
-                case "Send to Draft":
-                    campaignStatus.PostQueuedStatus = PostQueuedStatus.Draft;
+                    PostProcessOfStatusChange(publisherPostlistModel, post);
+                }
+                else if (statusToChange.ToString() == (string)Application.Current.FindResource("LangKeySendToDraft"))
+                {
+                    publisherPostlistModel.PostQueuedStatus = PostQueuedStatus.Draft;
                     post.PostQueuedStatus = PostQueuedStatus.Draft;
-                    PostProcessOfStatusChange(campaignStatus, post);
-                    break;
+                    PostProcessOfStatusChange(publisherPostlistModel, post);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.DebugLog();
             }
         }
 
@@ -736,6 +748,39 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
             PostlistFileManager.UpdatePost(campaignStatus.CampaignId, post);
             PublisherInitialize.GetInstance.UpdatePostStatus(campaignStatus.CampaignId);
             PublisherPostlist.Remove(campaignStatus);
+        }
+
+        public void PublishNow(PublisherPostlistModel postlistModel)
+        {
+            if (postlistModel != null)
+            {
+                try
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        PublishScheduler.StartPublishingPosts(postlistModel);
+                        //RefreshExecute(this);
+                    });
+                }
+                catch (OperationCanceledException ex)
+                {
+                    ex.DebugLog("Cancellation Requested!");
+                }
+                catch (AggregateException ae)
+                {
+                    foreach (var e in ae.InnerExceptions)
+                    {
+                        if (e is TaskCanceledException || e is OperationCanceledException)
+                            e.DebugLog("Cancellation requested before task completion!");
+                        else
+                            e.DebugLog(e.StackTrace + e.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.DebugLog();
+                }
+            }
         }
 
         #endregion
@@ -778,6 +823,7 @@ namespace DominatorUIUtility.ViewModel.SocioPublisher
             }
         }
 
+        
         #endregion
 
         #region Refresh 
