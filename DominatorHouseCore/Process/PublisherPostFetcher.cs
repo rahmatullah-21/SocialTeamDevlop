c1fd02b11aa2874fe3371886b6d05102f265ceab
Kamal <kamalkumar@globussoft.com>
2018-07-19 11:34:46 +0530

    Issues fixed in post fetching by Sathish

--- DominatorHouseCore/Process/PublisherPostFetcher.cs
+++ DominatorHouseCore/Process/PublisherPostFetcher.cs
@@ -36,96 +36,107 @@ namespace DominatorHouseCore.Process
 
         public void FetchPosts(PublisherPostFetchModel publisherPostFetchModel)
         {
-            if (publisherPostFetchModel.PostSource == PostSource.NormalPost)
-                return;
-
-            // Check whether campaign expired or not
-            if (publisherPostFetchModel.ExpireDate!=null && publisherPostFetchModel.ExpireDate < DateTime.Now)
+            try
             {
-                GlobusLogHelper.log.Info(
-                    $"{publisherPostFetchModel.CampaignName} expired on {publisherPostFetchModel.ExpireDate}");
-                return;
-            }
+                if (publisherPostFetchModel.PostSource == PostSource.NormalPost)
+                    return;
 
-            // Get all post lists and count 
-            var alreadyPresentedCount = PostlistFileManager.GetAll(publisherPostFetchModel.CampaignId).Count;
+                // Check whether campaign expired or not
+                if (publisherPostFetchModel.ExpireDate != null && publisherPostFetchModel.ExpireDate < DateTime.Now)
+                {
+                    GlobusLogHelper.log.Info(
+                        $"{publisherPostFetchModel.CampaignName} expired on {publisherPostFetchModel.ExpireDate}");
+                    return;
+                }
 
-            // Check already max posts has reached or not
-            if (alreadyPresentedCount >= publisherPostFetchModel.MaximumPostLimitToStore)
-            {
-                GlobusLogHelper.log.Info(
-                    $"{publisherPostFetchModel.CampaignName} have more than {publisherPostFetchModel.MaximumPostLimitToStore} posts in their postlist. Can't fetch new posts!");
-                return;
-            }
+                // Get all post lists and count 
+                var alreadyPresentedCount = PostlistFileManager.GetAll(publisherPostFetchModel.CampaignId).Count;
 
-            dynamic postFetchDetails = null;
+                // Check already max posts has reached or not
+                if (alreadyPresentedCount >= publisherPostFetchModel.MaximumPostLimitToStore)
+                {
+                    GlobusLogHelper.log.Info(
+                        $"{publisherPostFetchModel.CampaignName} have more than {publisherPostFetchModel.MaximumPostLimitToStore} posts in their postlist. Can't fetch new posts!");
+                    return;
+                }
 
-            // Collect neccessary details for fetching and assign to dynamic type variable
-            switch (publisherPostFetchModel.PostSource)
-            {
-                case PostSource.SharePost:
-                    postFetchDetails =
-                        JsonConvert.DeserializeObject<SharePostModel>(publisherPostFetchModel.PostDetailsWithFilters);
-                    break;
-                case PostSource.ScrapedPost:
-                    postFetchDetails =
-                        JsonConvert.DeserializeObject<ScrapePostModel>(publisherPostFetchModel.PostDetailsWithFilters);
-                    break;
-                case PostSource.RssFeedPost:
-                    postFetchDetails =
-                        JsonConvert.DeserializeObject<ObservableCollection<PublisherRssFeedModel>>(publisherPostFetchModel.PostDetailsWithFilters);
-                    break;
-                case PostSource.MonitorFolderPost:
-                    postFetchDetails =
-                        JsonConvert.DeserializeObject<ObservableCollection<PublisherMonitorFolderModel>>(publisherPostFetchModel.PostDetailsWithFilters);
-                    break;
-                case PostSource.NormalPost:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+                dynamic postFetchDetails = null;
 
-            // get the post scraper object for Rss feed and monitor folder
-            var postScraper = PublisherInitialize.GetPublisherLibrary(SocialNetworks.Social).GetPublisherCoreFactory()
-                .PostScraper.GetPostScraperLibrary();
+                // Collect neccessary details for fetching and assign to dynamic type variable
+                switch (publisherPostFetchModel.PostSource)
+                {
+                    case PostSource.SharePost:
+                        postFetchDetails =
+                            JsonConvert.DeserializeObject<SharePostModel>(publisherPostFetchModel.PostDetailsWithFilters);
+                        break;
+                    case PostSource.ScrapedPost:
+                        postFetchDetails =
+                            JsonConvert.DeserializeObject<ScrapePostModel>(publisherPostFetchModel.PostDetailsWithFilters);
+                        break;
+                    case PostSource.RssFeedPost:
+                        postFetchDetails =
+                            JsonConvert.DeserializeObject<ObservableCollection<PublisherRssFeedModel>>(publisherPostFetchModel.PostDetailsWithFilters);
+                        break;
+                    case PostSource.MonitorFolderPost:
+                        postFetchDetails =
+                            JsonConvert.DeserializeObject<ObservableCollection<PublisherMonitorFolderModel>>(publisherPostFetchModel.PostDetailsWithFilters);
+                        break;
+                    case PostSource.NormalPost:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
 
-            // Call the respective post scraper methods
-            switch (publisherPostFetchModel.PostSource)
-            {
-                case PostSource.RssFeedPost:
-                    JobManager.AddJob(() =>
-                     {
-                         postScraper.ScrapeRssPosts(publisherPostFetchModel.CampaignId, postFetchDetails);
-                     }, s => s.WithName($"{publisherPostFetchModel.CampaignId}-{PostSource.RssFeedPost.ToString()}").ToRunOnceAt(DateTime.Now.AddSeconds(2)).AndEvery(publisherPostFetchModel.DelayForNext).Minutes());
-                    break;
-                case PostSource.MonitorFolderPost:
-                    JobManager.AddJob(() =>
-                    {
-                        postScraper.FetchMonitorFoldersPosts(publisherPostFetchModel.CampaignId, postFetchDetails);
-                    }, s => s.WithName($"{publisherPostFetchModel.CampaignId}-{PostSource.RssFeedPost.ToString()}").ToRunOnceAt(DateTime.Now.AddSeconds(2)).AndEvery(publisherPostFetchModel.DelayForNext).Minutes());
-                    break;
-                case PostSource.NormalPost:
-                    break;
-                default:
-                    publisherPostFetchModel.SelectedDestinations.ToList().ForEach(destinationId =>
-                    {
-                        var destinationDetails = BinFileHelper.GetSingleDestination(destinationId);
+                // get the post scraper object for Rss feed and monitor folder
+                var postScraper = PublisherInitialize.GetPublisherLibrary(SocialNetworks.Social).GetPublisherCoreFactory()
+                    .PostScraper.GetPostScraperLibrary();
 
-                        destinationDetails.AccountsWithNetwork.ForEach(networkWithAccount =>
+                // Call the respective post scraper methods
+                switch (publisherPostFetchModel.PostSource)
+                {
+                    case PostSource.RssFeedPost:
+                        JobManager.AddJob(() =>
+                         {
+                             postScraper.ScrapeRssPosts(publisherPostFetchModel.CampaignId, postFetchDetails);
+                         }, s => s.WithName($"{publisherPostFetchModel.CampaignId}-{PostSource.RssFeedPost.ToString()}").ToRunOnceAt(DateTime.Now.AddSeconds(2)).AndEvery(publisherPostFetchModel.DelayForNext).Minutes());
+                        break;
+                    case PostSource.MonitorFolderPost:
+                        JobManager.AddJob(() =>
                         {
-                            var networkPostScraper = PublisherInitialize.GetPublisherLibrary(networkWithAccount.Key).GetPublisherCoreFactory()
-                                .PostScraper.GetPostScraperLibrary();
+                            postScraper.FetchMonitorFoldersPosts(publisherPostFetchModel.CampaignId, postFetchDetails);
+                        }, s => s.WithName($"{publisherPostFetchModel.CampaignId}-{PostSource.RssFeedPost.ToString()}").ToRunOnceAt(DateTime.Now.AddSeconds(2)).AndEvery(publisherPostFetchModel.DelayForNext).Minutes());
+                        break;
+                    case PostSource.NormalPost:
+                        break;
+                    default:
+                        publisherPostFetchModel.SelectedDestinations.ToList().ForEach(destinationId =>
+                        {
+                            var destinationDetails = BinFileHelper.GetSingleDestination(destinationId);
 
-                            if (publisherPostFetchModel.PostSource == PostSource.SharePost)
+                            destinationDetails.AccountsWithNetwork.ForEach(networkWithAccount =>
                             {
-                                if (networkWithAccount.Key == SocialNetworks.Facebook)
-                                    networkPostScraper.ScrapeFdPagePostUrl(networkWithAccount.Value, publisherPostFetchModel.CampaignId, postFetchDetails);
-                            }
-                            else if (publisherPostFetchModel.PostSource == PostSource.ScrapedPost)
-                                networkPostScraper.ScrapePosts(networkWithAccount.Value, publisherPostFetchModel.CampaignId, postFetchDetails);
+
+                                if(SocinatorInitialize.IsNetworkAvailable(networkWithAccount.Key))
+                                {
+                                    var networkPostScraper = PublisherInitialize.GetPublisherLibrary(networkWithAccount.Key).GetPublisherCoreFactory()
+                                   .PostScraper.GetPostScraperLibrary();
+
+                                    if (publisherPostFetchModel.PostSource == PostSource.SharePost)
+                                    {
+                                        if (networkWithAccount.Key == SocialNetworks.Facebook)
+                                            networkPostScraper.ScrapeFdPagePostUrl(networkWithAccount.Value, publisherPostFetchModel.CampaignId, postFetchDetails);
+                                    }
+                                    else if (publisherPostFetchModel.PostSource == PostSource.ScrapedPost)
+                                        networkPostScraper.ScrapePosts(networkWithAccount.Value, publisherPostFetchModel.CampaignId, postFetchDetails);
+                                }
+                            });
                         });
-                    });
-                    break;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.DebugLog();
             }
         }
     }
