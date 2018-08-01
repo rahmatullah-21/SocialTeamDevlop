841160681e866d62fca59409d3310e16c93c7dba
Basundhara Singh <basundharasingh@globussoft.in>
2018-08-01 11:23:44 +0530

    code commit

--- DominatorHouseCore/FileManagers/AccountsFileManager.cs
+++ DominatorHouseCore/FileManagers/AccountsFileManager.cs
@@ -77,10 +77,12 @@ namespace DominatorHouseCore.FileManagers
         {
             lstAccountModel.Clear();
             ApplyFunc(a =>
+           
             {
                 lstAccountModel.Add((T)(object)a);
                 return false;
             });
+            int i = 0;
         }
 
         public static List<DominatorAccountModel> GetAll()  => BinFileHelper.GetAccountDetails();
