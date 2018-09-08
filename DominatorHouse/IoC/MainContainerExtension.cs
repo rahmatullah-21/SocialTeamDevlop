e598ae8d9e54141158acbeb3d0f82d936d57b9ed
semashkin.vg <semashkin.vg@gmail.com>
2018-09-08 17:19:43 +0400

    added social module

---
+++ DominatorHouse/IoC/MainContainerExtension.cs
@@ -0,0 +1,21 @@
+﻿using DominatorHouseCore.Enums;
+using DominatorHouseCore.Interfaces;
+using DominatorUIUtility.IoC;
+using Socinator.Factories;
+using Unity;
+using Unity.Extension;
+
+namespace DominatorHouse
+{
+    public class MainContainerExtension : UnityContainerExtension
+    {
+        private const SocialNetworks CurrentyNetwork = SocialNetworks.Social;
+        protected override void Initialize()
+        {
+            // specify name of the module (CurrentyNetwork.ToString()) when you think it will be many registration upon your interface
+            Container.RegisterType<ISocialNetworkModule, MainDominatorModule>(CurrentyNetwork.ToString());
+            Container.RegisterType<INetworkCollectionFactory, SocialNetworkCollectionFactory>(CurrentyNetwork.ToString());
+            Container.RegisterType<IPublisherCollectionFactory, SocialPublisherCollectionFactory>(CurrentyNetwork.ToString());
+        }
+    }
+}
