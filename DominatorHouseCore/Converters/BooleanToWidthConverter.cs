0ed7aa81647314ff77a1ed842d68fff12fcf337e
semashkin.vg <semashkin.vg@gmail.com>
2018-10-20 20:41:43 +0400

    small converters cleanup

--- DominatorHouseCore/Converters/BooleanToWidthConverter.cs
+++ DominatorHouseCore/Converters/BooleanToWidthConverter.cs
@@ -1,57 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 
 namespace DominatorHouseCore.Converters
 {
-  
+
     public class BooleanToWidthConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType,object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return ((bool)value) ? 0 : int.Parse(parameter.ToString());
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-           return 0;
-        }
-    }
-    public class ProxyPortToIpAddressConvertor : IMultiValueConverter
-
-    {
-        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (values[0] == DependencyProperty.UnsetValue ||
-                values[1] == DependencyProperty.UnsetValue)
-            {
-                return String.Empty;
-            }
-            return String.Concat(values[0], ":", values[1]);
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return (value as string).Split(':');
-        }
-    }
-
-    public class ListConvertor : IMultiValueConverter
-
-    {
-        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            var list=new List<object>();
-            list.Add(values[0]);
-            list.Add(values[1]);
-            return list;
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return (value as string).Split(':');
+            return 0;
         }
     }
 }
