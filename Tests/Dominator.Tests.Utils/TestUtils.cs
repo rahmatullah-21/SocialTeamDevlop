357b935b420c08ae0a75040ca89190a3d40275e3
semashkin.vg <semashkin.vg@gmail.com>
2018-11-15 11:40:59 +0400

    unit tests + refactoring

---
+++ Tests/Dominator.Tests.Utils/TestUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Dominator.Tests.Utils
+{
+    public static class TestUtils
+    {
+        public static string ReadFileFromResources(string resourceName, Assembly assembly = null)
+        {
+
+            assembly = assembly ?? Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ Tests/Dominator.Tests.Utils/UnityInitializationTests.cs
@@ -0,0 +1,22 @@
+﻿using CommonServiceLocator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using Unity.ServiceLocation;
+
+namespace Dominator.Tests.Utils
+{
+    [TestClass]
+    public class UnityInitializationTests
+    {
+        protected IUnityContainer Container;
+
+        [TestInitialize]
+        public virtual void SetUp()
+        {
+            Container = new UnityContainer();
+            ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(Container));
+
+        }
+
+    }
+}
