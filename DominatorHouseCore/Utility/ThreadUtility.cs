b066bf2cd1569cd04d09babbf3d89fedddeb2f43
Oleg Krymskii <okrymsky@gmail.com>
2019-01-06 11:35:03 +0400

    Fix namespace for ThreadUtility

--- DominatorHouseCore/Utility/ThreadUtility.cs
+++ DominatorHouseCore/Utility/ThreadUtility.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TwtDominatorCore.TDUtility
+namespace DominatorHouseCore.Utility
 {
     public interface IThreadUtility
     {
