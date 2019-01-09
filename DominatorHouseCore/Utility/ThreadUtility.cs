de414efd4cd7ff283ce0e005db9af1be721857a7
semashkin.vg <semashkin.vg@gmail.com>
2019-01-09 10:59:56 +0400

    added the override for Sleep

--- DominatorHouseCore/Utility/ThreadUtility.cs
+++ DominatorHouseCore/Utility/ThreadUtility.cs
@@ -8,22 +8,27 @@ namespace DominatorHouseCore.Utility
     public interface IThreadUtility
     {
         void Sleep(int timeout);
+        void Sleep(TimeSpan timeout);
         Task Delay(int timeout, CancellationToken token);
     }
 
     [ExcludeFromCodeCoverage]
     public class ThreadUtility : IThreadUtility
     {
-         
+
         public void Sleep(int timeout)
+        {
+            Thread.Sleep(timeout);
+        }
+        public void Sleep(TimeSpan timeout)
         {
             Thread.Sleep(timeout);
         }
 
         public Task Delay(int timeout, CancellationToken token)
         {
-           return Task.Delay(timeout, token);
+            return Task.Delay(timeout, token);
         }
 
     }
-} 
+}
