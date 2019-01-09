fff1836245c212d53c654644ff27b573d4870a5d
Oleg Krymskii <okrymsky@gmail.com>
2019-01-09 10:57:50 +0400

    Add timespan to ThreadUtility

--- DominatorHouseCore/Utility/ThreadUtility.cs
+++ DominatorHouseCore/Utility/ThreadUtility.cs
@@ -8,16 +8,21 @@ namespace DominatorHouseCore.Utility
     public interface IThreadUtility
     {
         void Sleep(int timeout);
+        void Sleep(TimeSpan timeSpan);
         Task Delay(int timeout, CancellationToken token);
     }
 
     [ExcludeFromCodeCoverage]
     public class ThreadUtility : IThreadUtility
     {
-         
+
         public void Sleep(int timeout)
         {
             Thread.Sleep(timeout);
+        }
+        public void Sleep(TimeSpan timeSpan)
+        {
+            Thread.Sleep(timeSpan);
         }
 
         public Task Delay(int timeout, CancellationToken token)
