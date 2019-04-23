b90194b73023b1b9198f1f053f98900fbebbee0e
Kumar Harsh <kumarharsh@globussoft.in>
2019-04-23 17:54:57 +0530

    Added unit test for should_ToUserString_method_return_expactedresult_if_input_is_null, should_ToUserStringWithStack_method_return_expactedresult_if_input_is_null,should_ToUserStringWithStack_method_return_empty_string_if_input_is_not_null,should_ConvertValidHttpsUrl_method_convert_url_to_valid_http_url

---
+++ Tests/DominatorHouseCore.UnitTests/Tests/Utility/WebHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DominatorHouseCore.Requests;
+
+namespace DominatorHouseCore.UnitTests.Tests.Utility
+{
+    [TestClass]
+    public class WebHelperTest
+    {
+        [TestMethod]
+        public void should_ConvertValidHttpsUrl_method_convert_url_to_valid_http_url()
+        {
+            
+            string expactedresult = "https://www.google.com";
+            string data = "www.google.com";
+            var result = data.ConvertValidHttpsUrl();
+
+            result.Should().Be(expactedresult);
+        }
+    }
+}
