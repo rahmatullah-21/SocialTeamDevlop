7813cee7d1005b59bb8ac48b7f85de39edfccca4
Kumar Harsh <kumarharsh@globussoft.in>
2019-04-24 19:28:45 +0530

    Added unit test for AddOrUpdateModuleConfig_method_should_add_module_configuration_if_not_exist,AddOrUpdateModuleConfig_method_should_update_module_configuration_if_exist, AddOrUpdateModuleConfig_method_should_throw_NullReferenceException_if_moduleConfig_is_null,DeleteModuleConfig_method_should_delete_module_configuration_if_exist,DeleteModuleConfig_method_should_not_delete_module_configuration_if_not_exist

--- Tests/DominatorHouseCore.UnitTests/Tests/Utility/WebHelperTest.cs
+++ Tests/DominatorHouseCore.UnitTests/Tests/Utility/WebHelperTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DominatorHouseCore.Requests;
+using System.Net;
 
 namespace DominatorHouseCore.UnitTests.Tests.Utility
 {
@@ -11,12 +12,19 @@ namespace DominatorHouseCore.UnitTests.Tests.Utility
         [TestMethod]
         public void should_ConvertValidHttpsUrl_method_convert_url_to_valid_http_url()
         {
-            
-            string expactedresult = "https://www.google.com";
+
+            string validUrl = "https://www.google.com";
             string data = "www.google.com";
             var result = data.ConvertValidHttpsUrl();
 
-            result.Should().Be(expactedresult);
+            result.Should().Be(validUrl);
+        }
+        [TestMethod]
+        public void should_IsValidUriString_method_should_return_true_if_url_is_valid()
+        {
+            string url = "https://www.google.com";
+            var result = url.IsValidUriString();
+            result.Should().BeTrue();
         }
     }
 }
