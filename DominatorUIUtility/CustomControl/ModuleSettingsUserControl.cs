9df73e547c00f6f636eba89fb827163b15dd1467
semashkin <semashkin.vg@gmail.com>
2019-08-06 23:14:22 +0400

    added method to get rid of duplicates

--- DominatorUIUtility/CustomControl/ModuleSettingsUserControl.cs
+++ DominatorUIUtility/CustomControl/ModuleSettingsUserControl.cs
@@ -397,6 +397,19 @@ namespace DominatorUIUtility.CustomControl
 
         protected virtual bool ValidateExtraProperty() => true;
 
+        protected bool ValidateSavedQueries()
+        {
+            // Check queries
+            if (Model.SavedQueries.Count == 0)
+            {
+                DialogCoordinator.Instance.ShowModalMessageExternal(this, "Error", "Please add at least one query.",
+                    MessageDialogStyle.Affirmative);
+                return false;
+            }
+
+            return true;
+        }
+
         protected virtual bool ValidateQuery()
         {
             if (Model.SavedQueries.Count == 0)
