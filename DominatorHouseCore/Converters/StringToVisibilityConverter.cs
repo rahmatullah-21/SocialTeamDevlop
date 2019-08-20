a66fe54e06d4f8718ad40c493b032852bb66bc99
Somnath Sarkar <somnathsarkar@globussoft.in>
2019-08-20 23:05:08 +0530

    ++ Made changes for create account module

---
+++ DominatorHouseCore/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DominatorHouseCore.Converters
+{
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public bool IsInversed { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var visibility = string.IsNullOrEmpty(value?.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+
+            if (IsInversed)
+            {
+                visibility = string.IsNullOrEmpty(value?.ToString()) ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var valu = (bool)value;
+                return valu ? "Active" : "Paused";
+            }
+            catch (Exception ex)
+            {
+                ex.DebugLog();
+                return value;
+            }
+        }
+    }
+}
