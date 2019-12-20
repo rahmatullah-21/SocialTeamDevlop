9180a726d481fc455ce466f294995ada77f0d0b5
atul_globus <atulchaurasiya@globussoft.in>
2019-12-20 17:22:05 +0530

    ++Added AutoITTool Library, ++ Added copyAccountDetail Button in AccountDetails UI.

---
+++ DominatorHouseCore/Enums/KeyboardKeys.cs
@@ -0,0 +1,15 @@
+﻿namespace DominatorHouseCore.Enums
+{
+    public enum KeyboardKeys
+    {
+        Tab,
+        Enter,
+        Left,
+        Right,
+        Up,
+        Down,
+        A,
+        W,
+        C
+    }
+}
