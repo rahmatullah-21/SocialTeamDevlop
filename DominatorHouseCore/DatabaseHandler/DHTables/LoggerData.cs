8742b526117cd4f18e759c821f1fed00dac45556
Somnath Sarkar <somnathsarkar@globussoft.in>
2020-01-02 19:04:18 +0530

    ++ Added table for log files

---
+++ DominatorHouseCore/DatabaseHandler/DHTables/LoggerData.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+
+namespace DominatorHouseCore.DatabaseHandler.DHTables
+{
+    public class LoggerData
+    {
+        [PrimaryKey]
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 1)]
+        [Indexed]
+        [AutoIncrement]
+        public int Id { get; set; }
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 2)]
+        public DateTime DateTime { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 3)]
+        public DateTime Network { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 4)]
+        public DateTime AccountCampaign { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 5)]
+        public DateTime ActivityType { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 6)]
+        public DateTime Message { get; set; }
+    }
+}
