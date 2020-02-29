6c5210a2726dd2ead134304a5dd286239d643fc3
SomnathSarkar049 <somnathsarkar@globussoft.in>
2020-02-29 19:09:21 +0530

    ++ Made changes for Location model

--- DominatorHouseCore/Models/LocationModel.cs
+++ DominatorHouseCore/Models/LocationModel.cs
@@ -1,4 +1,5 @@
-﻿using ProtoBuf;
+﻿using DominatorHouseCore.Utility;
+using ProtoBuf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,51 @@ using System.Threading.Tasks;
 namespace DominatorHouseCore.Models
 {
     [ProtoContract]
-    public class LocationModel
+    public class LocationModel : BindableBase
     {
+        private string _countryName = string.Empty;
+        private string _cityName = string.Empty;
+        private bool _iSelected = false;
+
 
         [ProtoMember(1)]
-        public string CountryName { get; set; }
+        public string CountryName
+        {
+            get
+            {
+                return _countryName;
+            }
+            set
+            {
+                SetProperty(ref _countryName, value, nameof(CountryName));
+            }
+        }
 
         [ProtoMember(2)]
-        public string CityName { get; set; }
+        public string CityName
+        {
+            get
+            {
+                return _cityName;
+            }
+            set
+            {
+                SetProperty(ref _cityName, value, nameof(CityName));
+            }
+        }
 
         [ProtoMember(3)]
-        public bool IsSelected { get; set; }
+        public bool IsSelected
+        {
+            get
+            {
+                return _iSelected;
+            }
+            set
+            {
+                SetProperty(ref _iSelected, value, nameof(IsSelected));
+            }
+        }
 
 
     }
