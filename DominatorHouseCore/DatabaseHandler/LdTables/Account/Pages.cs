46a92e23c765fd7ba981dc8b2d6efff5fd88fa48
Somnath049 <50858978+Somnath049@users.noreply.github.com>
2020-04-02 12:01:28 +0530

    ++ Made changes for Linkedin

---
+++ DominatorHouseCore/DatabaseHandler/LdTables/Account/Pages.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+
+namespace DominatorHouseCore.DatabaseHandler.LdTables.Account
+{
+    public class Pages
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Indexed]
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 1)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Contains Name Of the Group
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 2)]
+        public string CompanyPageName
+        { get; set; }
+
+        /// <summary>
+        /// Contains Url Of the Group
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 3)]
+        //[Unique]
+        public string CompanyPageUrl
+        { get; set; }
+
+        /// <summary>
+        /// Contains Profile Picture Url Of the Connection
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 4)]
+        public string TotalMembers { get; set; }
+
+        /// <summary>
+        /// Describe Connection Type If FirstDegree,SecondDegree Or ThirdPlusDegree
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 5)]
+        public string CompanyUniversalName
+        { get; set; }
+        /// <summary>
+        /// Contains Interaction Time Stamp
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Schema.Column(Order = 6)]
+        public int InteractionTimeStamp { get; set; }
+    }
+}
