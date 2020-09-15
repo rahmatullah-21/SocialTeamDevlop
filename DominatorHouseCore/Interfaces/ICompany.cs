40bc38876778c6152c612a64950cd4b99a861f38
atul_globus <atulchaurasiya@globussoft.in>
2020-09-15 18:49:50 +0530

    ++ Released version 1.0.0.93 ++  Some changes added in ICompany interface

--- DominatorHouseCore/Interfaces/ICompany.cs
+++ DominatorHouseCore/Interfaces/ICompany.cs
@@ -4,5 +4,10 @@
     {
         string CompanyId { get; set; }
         string CompanyUrl { get; set; }
+        string CompanyName { get; set; }
+        string TotalEmployees { get; set; }
+        string Industry { get; set; }
+        string Location { get; set; }
+        string IsFollowed { get; set; }
     }
 }
