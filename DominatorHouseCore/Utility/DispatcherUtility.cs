bbe95c01793457e311861a91e9fdff5480814a52
semashkin <semashkin.vg@gmail.com>
2020-10-07 21:19:19 +0400

    added sync method

--- DominatorHouseCore/Utility/DispatcherUtility.cs
+++ DominatorHouseCore/Utility/DispatcherUtility.cs
@@ -7,6 +7,7 @@ namespace DominatorHouseCore.Utility
     public interface IDispatcherUtility
     {
         DispatcherOperation InvokeAsync(Action callback);
+        void Invoke(Action callback);
     }
 
     public class DispatcherUtility : IDispatcherUtility
@@ -14,6 +15,11 @@ namespace DominatorHouseCore.Utility
         public DispatcherOperation InvokeAsync(Action callback)
         {
             return Application.Current.Dispatcher.InvokeAsync(callback);
+        }
+
+        public void Invoke(Action callback)
+        {
+            Application.Current.Dispatcher.Invoke(callback);
         }
     }
 }
